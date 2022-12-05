d39ec1bcc77e00341df4db939b06efce141d8873
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-05 20:05:59 +0530

    welcome

--- src/main/java/com/example/employee_backend/controller/EmployeeBackend.java
+++ src/main/java/com/example/employee_backend/controller/EmployeeBackend.java
@@ -1,10 +1,17 @@
 package com.example.employee_backend.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeBackend {
+
+    @GetMapping("/")
+    public String welcomepage(){
+        return "welcome";
+    }
+
 
 
 
